a8e68f97bbab162ccbad0a97156927d92d59a83f
Rogers Paredes <rparedes@todo1.com>
2019-08-05 10:52:38 -0500

    first commit

--- Georeferenciacion/Pods/Target Support Files/BCAFNetworking/BCAFNetworking-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_BCAFNetworking : NSObject
-@end
-@implementation PodsDummy_BCAFNetworking
-@end
--- GeoreferenciacionSmartWatch/Pods/Target Support Files/Pods-GeoreferenciacionSmartWatch/Pods-GeoreferenciacionSmartWatch-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GeoreferenciacionSmartWatch : NSObject
-@end
-@implementation PodsDummy_Pods_GeoreferenciacionSmartWatch
-@end
